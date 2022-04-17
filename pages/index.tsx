96e5655b249034cb1a073d2bf9405a026806536c
Konstantin Auffinger <konstantinauffinger@gmail.com>
2022-04-17 19:54:47 +0200

    add default page styles

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="mx-auto">
+      <h1>Hi There</h1>
+    </div>
+  );
+};
+
+export default Home;
