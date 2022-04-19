3d1fa640b4930cbfc11da109af517f801ad7c033
Konstantin Auffinger <konstantinauffinger@gmail.com>
2022-04-19 11:40:15 +0200

    Removed fallback for AuthGate

--- components/auth/AuthGate.tsx
+++ components/auth/AuthGate.tsx
@@ -1,26 +1,21 @@
+import Section from "../layout/Section";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../lib/context";
 
-function AuthGate({
-  fallback,
-  children,
-}: {
-  fallback?: ReactNode;
-  children: ReactNode;
-}) {
+function AuthGate({ children }: { children: ReactNode }) {
   const { user } = useContext(UserContext);
   return user ? (
     <>{children}</>
   ) : (
-    <>{fallback}</> || (
-      <div className="flex flex-col h-screen justify-center">
+    <Section>
+      <div className="flex flex-col justify-center h-48">
         <div className="text-center text-2xl font-bold">
           You must be logged in to view this.
         </div>
       </div>
-    )
+    </Section>
   );
 }
 
